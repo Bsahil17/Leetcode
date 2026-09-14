c69b5789301d6a9069077ccb30d9c2dfd8aa1171
B Sahil Madival <bsahilmadival@gmail.com>
2026-09-14 21:51:33 +0530

    Time: 1 ms (90.85%) | Memory: 42.3 MB (54.24%) - LeetSync

---
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumSum(int num) {
+    int ans[]=new int[4];
+    for(int i=0;i<4;i++){
+        ans[i]=num%10;
+        num=num/10;
+    }
+    Arrays.sort(ans);
+    int num1=ans[0]*10+ans[2];
+    int num2=ans[1]*10+ans[3];
+    return num1+num2;
+    }
+}
